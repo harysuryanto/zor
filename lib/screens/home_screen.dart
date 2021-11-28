4707471440e3b0ee5a5660745ad0031fcacfa01d
Hary Suryanto <hary.suryanto01@gmail.com>
2021-11-28 21:28:40 +0700

    add home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:zor/widgets/exercise_list.dart';
+import 'package:zor/widgets/news_carousel.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  top: 60,
+                  right: 30,
+                  bottom: 30,
+                  left: 30,
+                ),
+                child: Text(
+                  'Senang melihatmu kembali!',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+
+              /// News Carousel
+              NewsCarousel(key: UniqueKey()),
+
+              const SizedBox(height: 30),
+
+              /// List of Exercise
+              const ExerciseList(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
