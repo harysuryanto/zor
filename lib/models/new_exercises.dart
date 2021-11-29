3bba48383eec6c36d0ff0e64b3dd28af8d38c49c
Hary Suryanto <hary.suryanto01@gmail.com>
2021-11-30 01:48:04 +0700

    create temporary data for add new plan

---
+++ lib/models/new_exercises.dart
@@ -0,0 +1,54 @@
+class Excercise {
+  String name;
+  String reps;
+  String restTime;
+
+  Excercise({
+    required this.name,
+    required this.reps,
+    required this.restTime,
+  });
+}
+
+final newExercises = [
+  Excercise(
+    name: 'Jumping Jack',
+    reps: '30 rep',
+    restTime: '5 detik',
+  ),
+  Excercise(
+    name: 'Plank',
+    reps: '10 detik',
+    restTime: '60 detik',
+  ),
+  Excercise(
+    name: 'Jumping Jack',
+    reps: '30 rep',
+    restTime: '5 detik',
+  ),
+  Excercise(
+    name: 'Plank',
+    reps: '10 detik',
+    restTime: '60 detik',
+  ),
+  Excercise(
+    name: 'Jumping Jack',
+    reps: '30 rep',
+    restTime: '5 detik',
+  ),
+  Excercise(
+    name: 'Plank',
+    reps: '10 detik',
+    restTime: '60 detik',
+  ),
+  Excercise(
+    name: 'Jumping Jack',
+    reps: '30 rep',
+    restTime: '5 detik',
+  ),
+  Excercise(
+    name: 'Plank',
+    reps: '10 detik',
+    restTime: '60 detik',
+  ),
+];
