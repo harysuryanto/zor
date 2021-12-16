a03bb3803a740d563e3ec5cd10da5a331e69ad85
Hary Suryanto <hary.suryanto01@gmail.com>
2021-12-17 00:09:36 +0700

    add custom button

---
+++ lib/widgets/small/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  void Function()? onPressed;
+
+  CustomButton({
+    Key? key,
+    required this.title,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(title),
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+      ),
+    );
+  }
+}
