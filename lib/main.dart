2b8bb3aff738d3a8ade71d90de38ffe6d52da5e0
Hary Suryanto <hary.suryanto01@gmail.com>
2022-01-16 13:49:05 +0700

    migrate Firebase from manual instalation to CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,75 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:zor/firebase_options.dart';
 
+import 'providers/auth.dart';
 import 'screens/add_plan_screen.dart';
 import 'screens/detail_plan_screen.dart';
 import 'screens/exercising_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zor',
-      theme: FlexThemeData.light(scheme: FlexScheme.deepBlue),
-      darkTheme: FlexThemeData.dark(scheme: FlexScheme.deepBlue),
-      themeMode: ThemeMode.light,
-      routes: {
-        '/': (context) => const HomeScreen(),
-        '/detail-plan': (context) => const DetailPlanScreen(),
-        '/add-plan': (context) => AddPlanScreen(),
-        '/exercising': (context) => const ExercisingScreen(),
+    final auth = Auth();
+
+    return StreamBuilder<User?>(
+      stream: auth.streamAuthStatus,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          // print(snapshot.data);
+
+          return MaterialApp(
+            key: UniqueKey(),
+            title: 'Zor',
+            theme: FlexThemeData.light(scheme: FlexScheme.deepBlue),
+            darkTheme: FlexThemeData.dark(scheme: FlexScheme.deepBlue),
+            themeMode: ThemeMode.light,
+            initialRoute: snapshot.data != null &&
+                    (snapshot.data!.emailVerified || snapshot.data!.isAnonymous)
+                ? '/home'
+                : '/login',
+            routes: {
+              '/login': (context) => const LoginScreen(),
+              '/home': (context) => const HomeScreen(),
+              '/detail-plan': (context) => const DetailPlanScreen(),
+              '/add-plan': (context) => AddPlanScreen(),
+              '/exercising': (context) => const ExercisingScreen(),
+            },
+          );
+        }
+
+        return const LoadingScreen();
       },
+    );
+  }
+}
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
