1ce455283ae2116aff8b80f762cf435cd4905c23
Hary Suryanto <hary.suryanto01@gmail.com>
2022-01-16 13:16:13 +0700

    add colors utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(235, 87, 87, 1);
+const whiteColor = Colors.white;
+const blackColor = Colors.black;
+const lightColor = Color.fromRGBO(245, 245, 245, 1);
+const darkColor = Color.fromRGBO(54, 54, 54, 1);
