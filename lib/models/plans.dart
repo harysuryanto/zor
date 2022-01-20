7ed739d7f23484c6e29e451c44fbcaf7a6deca01
Hary Suryanto <hary.suryanto01@gmail.com>
2022-01-20 23:25:27 +0700

    add method to get more data

--- lib/models/plans.dart
+++ lib/models/plans.dart
@@ -1,13 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class Plans {
-  FirebaseAuth instance = FirebaseAuth.instance;
+  FirebaseFirestore instance = FirebaseFirestore.instance;
 
-  /// Create a CollectionReference called users
-  /// that references the firestore collection
-  CollectionReference plans = FirebaseFirestore.instance
-      .collection('users')
-      .doc('user1')
-      .collection('plans');
+  DocumentReference get currentUser {
+    /// TODO: ganti [user1] menjadi dinamis
+    return instance.collection('users').doc('user1');
+  }
+
+  CollectionReference get plans {
+    return currentUser.collection('plans');
+  }
+
+  CollectionReference exercises({required String planId}) {
+    final exercises = plans.doc(planId).collection('exercises');
+    return exercises;
+  }
 }
