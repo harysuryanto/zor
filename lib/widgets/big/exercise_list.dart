473f9e2b087bd091909d10e344d9aa5c81432956
Hary Suryanto <hary.suryanto01@gmail.com>
2022-01-22 23:45:19 +0700

    fix ui to match the mockup

--- lib/widgets/big/exercise_list.dart
+++ lib/widgets/big/exercise_list.dart
@@ -14,46 +14,61 @@ class ExerciseList extends StatelessWidget {
   Widget build(BuildContext context) {
     final exercises = Database().exercises(planId: planId);
 
-    return Padding(
+    return FutureBuilder<QuerySnapshot>(
+      future: exercises.get(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Terjadi kesalahan.');
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          dynamic data = (snapshot.data! as dynamic).docs;
+
+          return _buildTimeline(data);
+        }
+
+        return const Text("loading");
+      },
+    );
+  }
+
+  Widget _buildTimeline(dynamic data) {
+    return Timeline.tileBuilder(
+      builder: TimelineTileBuilder(
+        indicatorBuilder: (_, index) => Indicator.outlined(
+          key: ValueKey('timeline indicator ${data[index].id}'),
+          position: 0,
+          borderWidth: 1,
+          color: primaryColor,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text('${index + 1}'),
+          ),
+        ),
+        endConnectorBuilder: (_, index) => index == data.length - 1
+            ? null
+            : Padding(
+                key: ValueKey('timeline connector ${data[index].id}'),
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Connector.solidLine(thickness: 1, color: Colors.grey),
+              ),
+        contentsBuilder: (_, index) {
+          return Padding(
+            key: ValueKey('timeline content ${data[index].id}'),
+            padding: const EdgeInsets.only(
+              left: 20,
+              bottom: 10,
+            ),
+            child: PlanListTile(
+              title: data[index]['name'],
+            ),
+          );
+        },
+        itemCount: data.length,
+        contentsAlign: ContentsAlign.basic,
+        nodePositionBuilder: (_, __) => 0,
+      ),
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: FutureBuilder<QuerySnapshot>(
-          future: exercises.get(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return const Text('Terjadi kesalahan.');
-            }
-
-            if (snapshot.connectionState == ConnectionState.done) {
-              dynamic data = (snapshot.data! as dynamic).docs;
-
-              return Timeline.tileBuilder(
-                builder: TimelineTileBuilder.fromStyle(
-                  contentsBuilder: (context, index) => Padding(
-                    key: ValueKey(data[index].id),
-                    padding: const EdgeInsets.only(
-                      left: 20,
-                      bottom: 10,
-                    ),
-                    child: PlanListTile(
-                      title: data[index]['name'],
-                    ),
-                  ),
-                  itemCount: data.length,
-                  nodePositionBuilder: (_, __) => 0,
-                  contentsAlign: ContentsAlign.basic,
-                  connectorStyle: ConnectorStyle.dashedLine,
-                ),
-                theme: TimelineThemeData(
-                  connectorTheme:
-                      const ConnectorThemeData(color: Colors.black12),
-                  indicatorTheme: const IndicatorThemeData(color: primaryColor),
-                ),
-              );
-            }
-
-            return const Text("loading");
-          }),
     );
   }
 }
