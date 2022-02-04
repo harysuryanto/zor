19d4046baa726349a3a0ee1d878ac08b240eae77
Hary Suryanto <hary.suryanto01@gmail.com>
2022-02-04 23:29:04 +0700

    move documentation text placement

--- lib/widgets/small/navigator_wrapper.dart
+++ lib/widgets/small/navigator_wrapper.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class NavigatorWrapper extends StatefulWidget {
+  final Widget child;
+
   /// This NavigatorWrapper widget was made to solve an error
   /// where a TextField needs to be inside of MaterialApp() and not
   /// MaterialApp.router() or any other MaterialApp-like widget.
   /// I use MaterialApp.router() because go_router forced us to use it.
-
-  final Widget child;
 
   const NavigatorWrapper({required this.child, Key? key}) : super(key: key);
 
