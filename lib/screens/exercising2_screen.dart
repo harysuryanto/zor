d7a7d5e3dd67c6822d93e280b7933043bd7ddcc6
Hary Suryanto <hary.suryanto01@gmail.com>
2022-02-09 22:03:20 +0700

    change illustration and add progress indicator to set counter

--- lib/screens/exercising2_screen.dart
+++ lib/screens/exercising2_screen.dart
@@ -1,5 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 import '../utils/colors.dart';
 
@@ -12,6 +13,35 @@ class Exercising2Screen extends StatefulWidget {
 }
 
 class _Exercising2ScreenState extends State<Exercising2Screen> {
+  int _currentSetProgress = 33; // Range of 0 to 100
+
+  void setCurrentSetProgressTo(int newValue) async {
+    /// This method gives animation to the CircularStepProgressIndicator
+
+    int startValue = _currentSetProgress;
+    int finishValue = newValue;
+    List<int> progress;
+
+    /// Check if the progress is ascending or descending
+    if (startValue < finishValue) {
+      /// Ascending
+      /// Output: [1, 2, 3]
+      progress = [for (var i = startValue; i < finishValue; i++) i];
+    } else {
+      /// Descending
+      /// Output: [3, 2, 1]
+      progress = [for (var i = startValue; i > finishValue; i--) i];
+    }
+
+    /// Progressively change value of [_currentSetProgress]
+    for (var value = 0; value < progress.length; value++) {
+      setState(() {
+        _currentSetProgress = progress[value];
+      });
+      await Future.delayed(const Duration(milliseconds: 1));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,22 +54,14 @@ class _Exercising2ScreenState extends State<Exercising2Screen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  /// TODO: Got error, fix this.
                   /// Illustration
-                  // Image.asset(
-                  //   "assets/illustrations/man_doing_dumble_curle.png",
-                  //   width: 180,
-                  // ),
-                  CachedNetworkImage(
-                    imageUrl: "https://iili.io/l01tEP.png",
-                    width: 180,
-                    placeholder: (context, url) =>
-                        const Center(child: CircularProgressIndicator()),
-                    errorWidget: (context, url, error) =>
-                        const Center(child: Icon(Icons.error)),
+                  Lottie.network(
+                    'https://assets3.lottiefiles.com/private_files/lf30_i5o0xxk6.json',
+                    height: 200,
+                    key: const ValueKey('Exercising illustration'),
                   ),
 
-                  const SizedBox(height: 50),
+                  const SizedBox(height: 20),
 
                   /// Texts
                   const Text(
@@ -89,26 +111,36 @@ class _Exercising2ScreenState extends State<Exercising2Screen> {
                       Container(
                         width: 120,
                         height: 120,
-                        alignment: Alignment.center,
                         decoration: const BoxDecoration(
                           color: lightColor,
                           borderRadius: BorderRadius.all(Radius.circular(80)),
                         ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: const [
-                            Text(
-                              '1/3',
-                              style: TextStyle(
-                                fontSize: 36,
-                                fontWeight: FontWeight.bold,
-                              ),
+                        child: CircularStepProgressIndicator(
+                          key: const ValueKey('Set progress indicator'),
+                          totalSteps: 100,
+                          currentStep: _currentSetProgress,
+                          selectedColor: primaryColor,
+                          unselectedColor: Colors.transparent,
+                          roundedCap: (_, __) => true,
+                          stepSize: 7,
+                          child: Center(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Text(
+                                  '1/3',
+                                  style: TextStyle(
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'set',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ],
                             ),
-                            Text(
-                              'set',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
                     ],
@@ -145,7 +177,9 @@ class _Exercising2ScreenState extends State<Exercising2Screen> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        setCurrentSetProgressTo(66);
+                      },
                       style: ButtonStyle(
                         backgroundColor:
                             MaterialStateProperty.all<Color>(primaryColor),
