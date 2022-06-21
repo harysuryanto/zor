960f23eeadfdd6db9db3174d07947589a1bf8f6f
Hary Suryanto <hary.suryanto01@gmail.com>
2022-06-21 14:05:32 +0700

    add a parameter named adPlacement

--- lib/widgets/global/banner_ad.dart
+++ lib/widgets/global/banner_ad.dart
@@ -1,10 +1,22 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+enum AdPlacement {
+  homeScreen,
+  allPlansScreen,
+  detailPlanScreen,
+}
+
 class AdBanner extends StatefulWidget {
-  const AdBanner({Key? key}) : super(key: key);
+  const AdBanner({
+    Key? key,
+    this.adPlacement = AdPlacement.homeScreen,
+  }) : super(key: key);
+
+  final AdPlacement adPlacement;
 
   @override
   State<AdBanner> createState() => _AdBannerState();
@@ -17,29 +29,18 @@ class _AdBannerState extends State<AdBanner> {
   @override
   void initState() {
     super.initState();
-    _initBannerAd();
-  }
-
-  _initBannerAd() {
-    _bannerAd = BannerAd(
-      size: AdSize.banner,
-      adUnitId: BannerAd.testAdUnitId,
-      listener: BannerAdListener(
-        onAdLoaded: (ad) {
-          setState(() => _isAdLoaded = true);
-        },
-        onAdFailedToLoad: (ad, error) {},
-      ),
-      request: const AdRequest(),
-    );
-
+    _initializeAd();
     _bannerAd.load();
   }
 
   @override
-  Widget build(BuildContext context) {
-    log('BannerAd rendered');
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     if (_isAdLoaded) {
       return SizedBox(
         height: _bannerAd.size.height.toDouble(),
@@ -49,5 +50,41 @@ class _AdBannerState extends State<AdBanner> {
     }
 
     return const SizedBox();
+  }
+
+  String _adUnitIdAndroid(AdPlacement adPlacement) {
+    switch (adPlacement) {
+      case AdPlacement.homeScreen:
+        return 'ca-app-pub-9675217052405779/4840390767';
+      case AdPlacement.allPlansScreen:
+        return 'ca-app-pub-9675217052405779/9697965774';
+      case AdPlacement.detailPlanScreen:
+        return 'ca-app-pub-9675217052405779/4717101834';
+      default:
+        return _adUnitIdAndroid(AdPlacement.homeScreen);
+    }
+  }
+
+  String _adUnitIdIos(AdPlacement adPlacement) {
+    // Currently ads are not supported on iOS.
+    // So it returns adUnitId from AdPlacement.homeScreen for Android.
+    return _adUnitIdAndroid(AdPlacement.homeScreen);
+  }
+
+  _initializeAd() {
+    _bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: Platform.isAndroid
+          ? _adUnitIdAndroid(widget.adPlacement)
+          : _adUnitIdIos(widget.adPlacement),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) => setState(() => _isAdLoaded = true),
+        onAdFailedToLoad: (ad, error) {
+          log('Ad failed to load: ${error.message}');
+          _bannerAd.dispose();
+        },
+      ),
+      request: const AdRequest(),
+    );
   }
 }
