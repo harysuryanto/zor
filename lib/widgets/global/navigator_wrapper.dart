d8df4688405b11d3f9663e8c25ae1e1a508a7f15
Hary Suryanto <hary.suryanto01@gmail.com>
2022-07-03 10:21:34 +0700

    refactor code

--- lib/widgets/global/navigator_wrapper.dart
+++ lib/widgets/global/navigator_wrapper.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 
-class NavigatorWrapper extends StatefulWidget {
+/// This NavigatorWrapper widget was made to solve an error
+/// where a TextField needs to be inside of MaterialApp() and not
+/// MaterialApp.router() or any other MaterialApp-like widget.
+/// I use MaterialApp.router() because go_router forced us to use it.
+class NavigatorWrapper extends StatelessWidget {
   final Widget child;
 
-  /// This NavigatorWrapper widget was made to solve an error
-  /// where a TextField needs to be inside of MaterialApp() and not
-  /// MaterialApp.router() or any other MaterialApp-like widget.
-  /// I use MaterialApp.router() because go_router forced us to use it.
+  const NavigatorWrapper({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
 
-  const NavigatorWrapper({required this.child, Key? key}) : super(key: key);
-
-  @override
-  State<NavigatorWrapper> createState() => _NavigatorWrapperState();
-}
-
-class _NavigatorWrapperState extends State<NavigatorWrapper> {
   @override
   Widget build(BuildContext context) {
     return Navigator(
       onGenerateRoute: (_) => MaterialPageRoute(
-        builder: (_) => widget.child,
+        builder: (_) => child,
       ),
     );
   }
